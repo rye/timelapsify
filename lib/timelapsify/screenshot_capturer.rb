85f9474e910b5bdf9578cda00514775e29050f40
Kristofer Rye <kristofer.rye@gmail.com>
2014-09-29 17:20:51 -0500

    Remove unnecessary comment.

--- lib/timelapsify/screenshot_capturer.rb
+++ lib/timelapsify/screenshot_capturer.rb
@@ -1,7 +1,5 @@
 require "timelapsify/rmagick_screenshot_capturer"
 
-# Magick::Image.capture(true, false, false, true, true) {self.filename = "root"}.write("test.png")
-
 module Timelapsify
 	class ScreenshotCapturer
 		def initialize(utility = :rmagick)
